97719d0d31409cd823a88fb775c0a19ad9ab0e43
Connorw2222 <Connor2Werth@gmail.com>
2021-08-23 12:12:53 -0700

    added in the read me

--- Develop/script.js
+++ Develop/script.js
@@ -5,50 +5,58 @@ $(document).ready(function () {
 
     // create time blocks
     var loopArray = [
-        "9", "10", "11", "12", "1", "2", "3", "4", "5", "6"
+        "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"
     ]
-    //StorageArray
-    var storageArray = []
-    //creates elements consol log plannerNotes
     for (let index = 0; index < loopArray.length; index++) {
+        localStorage.getItem(loopArray[index]) ? null : localStorage.setItem(loopArray[index], "")
         var timeBlocks = $("<div>").addClass("timeBlocks").attr("id", loopArray[index])
-        timeBlocks.append($("<sect>").addClass("timeZone").text(loopArray[index]).attr("id",loopArray[index]+"timeZone"));
-        timeBlocks.append($("<textarea>").addClass("plannerNotes").text("notes").attr("id",loopArray[index]+"plannerNotes"));
-        timeBlocks.append($("<button>").addClass("save").text("Save").click(function () {
-            var val = $("#"+loopArray[index]+"plannerNotes").val();
-            console.log(val)
-            console.log($(this).siblings())
-            var timeSaveLS = $(this).siblings("")
-            console.log(timeSaveLS)
+        timeBlocks.append($("<section>").addClass("timeZone").text(loopArray[index]).attr("id", loopArray[index] + "timeZone"));
+        timeBlocks.append($("<textarea>").addClass("plannerNotes").attr("id", loopArray[index] + "plannerNotes").addClass(" row container-fluid m-1"));
+        timeBlocks.append($("<button>").addClass("save").text("Save").addClass("row m-1 btn btn-primary btn-lg btn-block active").click(function () {
+            let val;
+            let key;
+            console.log($("#" + loopArray[index] + "plannerNotes").val())
+            console.log($("#" + loopArray[index] + "plannerNotes").siblings(".timeZone").text())
+            val = $("#" + loopArray[index] + "plannerNotes").val()
+            key = $("#" + loopArray[index] + "plannerNotes").siblings(".timeZone").text()
+            localStorage.setItem(key, val)
+
         }))
         $(".container").append(timeBlocks)
     }
-    //setting up local storage
-    localStorage.setItem("storageArray",storageArray)
+    // get from local storage
+
+    // loads Local storage on page
+    function loadLS() {
+        for (let i = 0; i < loopArray.length; i++) {
+            $("#" + loopArray[i] + "plannerNotes").text(localStorage.getItem(loopArray[i]))
+
+        }
+    }
+    loadLS()
+
+    // For Colors
+    function color() {
+        var currentHour = moment().hours()
+        for (let i = 0; i < loopArray.length; i++) {
+            if (currentHour === loopArray[i]) {
+                $("#" + loopArray[i] + "plannerNotes").addClass("red")
+                $("#" + loopArray[i] + "plannerNotes").removeClass("grey")
+            }
+            else if (currentHour > loopArray[i]) {
+                $("#" + loopArray[i] + "plannerNotes").addClass("grey")
+            }
+            else {
+                $("#" + loopArray[i] + "plannerNotes").addClass("green")
+                $("#" + loopArray[i] + "plannerNotes").removeClass("red")
+                $("#" + loopArray[i] + "plannerNotes").removeClass("grey")
+            }
+        }
+    };
+    setInterval(color,36000)
+    color()
 
 
-
-    //saved
-    // Save btm conole log = text area value
-
-
-    // event listener http://html-tuts.com/jquery-this-selector/ worse case use event listern for every item
-
-    // Set up local storage
-
-    // comments in time appropate block; using indavidual blocks for local storage (10 total)
-
-    // on page load show all notes in approtiate* time blocks
-
-    //edit time blocks
-
-    //store in local storage! 
-
-    //delete local storaga after the work day is done! 
-
-    //color distiction for past prez and future. 
-
-    //css and bootstrap styling! 
 
 
 })
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Work Day Scheduler Starter Code
+# Work Day Planner
+
+## Instalation
+    Download and unzip the file to use.
+
+## Usage 
+    Desgined to allow better schedualing of the work day. 
+    
+### User Story
+    AS AN employee with a busy schedule
+    I WANT to add important events to a daily planner
+    SO THAT I can manage my time effectively
